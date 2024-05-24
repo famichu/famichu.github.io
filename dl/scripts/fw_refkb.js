c19c513d65c9de6005d7aaf2071aa7ad804d8c35
famichu <famichu@gmail.com>
2024-05-25 01:17:14 +0900

    Add reference keyboard contents

---
+++ dl/scripts/fw_refkb.js
@@ -0,0 +1,9 @@
+const a = document.createElement('a')
+a.href = "../files/magrev_switch_refkb_fw_1.00.uf2";
+a.download = "magrev_switch_refkb_fw_1.00.uf2";
+a.style.display = 'none'
+a.click();
+
+a.addEventListener('click', function () {
+    window.close();
+});
