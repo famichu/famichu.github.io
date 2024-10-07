f30fcdd64170f3a4740c1248e13a4de92f5fef7c
famichu <famichu@gmail.com>
2024-10-07 21:57:25 +0900

    add the binary of the reference keyboard fw v1.01

--- dl/scripts/fw_refkb.js
+++ dl/scripts/fw_refkb.js
@@ -1,6 +1,6 @@
 const a = document.createElement('a')
-a.href = "../files/magrev_switch_refkb_fw_1.00.uf2";
-a.download = "magrev_switch_refkb_fw_1.00.uf2";
+a.href = "../files/magrev_switch_refkb_fw_1.01.uf2";
+a.download = "magrev_switch_refkb_fw_1.01.uf2";
 a.style.display = 'none'
 a.click();
 
