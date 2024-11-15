cbbbac02dc27dd5452dcd58c5083a51ab0c08ce2
famichu <famichu@gmail.com>
2024-11-16 03:21:05 +0900

    update event info and manuals

--- dl/scripts/man_bm2.js
+++ dl/scripts/man_bm2.js
@@ -1,6 +1,6 @@
 const a = document.createElement('a')
-a.href = "../files/maglev_switch_breakout2_manual_1.00.pdf";
-a.download = "maglev_switch_breakout2_manual_1.00.pdf";
+a.href = "../files/maglev_switch_breakout2_manual_1.01.pdf";
+a.download = "maglev_switch_breakout2_manual_1.01.pdf";
 a.style.display = 'none'
 a.click();
 
--- dl/scripts/man_refkb.js
+++ dl/scripts/man_refkb.js
@@ -1,6 +1,6 @@
 const a = document.createElement('a')
-a.href = "../files/maglev_switch_refkb_manual_1.00.pdf";
-a.download = "maglev_switch_refkb_manual_1.00.pdf";
+a.href = "../files/maglev_switch_refkb_manual_1.01.pdf";
+a.download = "maglev_switch_refkb_manual_1.01.pdf";
 a.style.display = 'none'
 a.click();
 
