fbe9f8da792f7a005d64ec7c0085031e40da1174
Ryo Taketani <r-taketani@sunnyeg.co.jp>
2025-02-20 12:55:51 +0900

    add info of the keeb-market2025

---
+++ dl/scripts/man_bm3.js
@@ -0,0 +1,9 @@
+const a = document.createElement('a')
+a.href = "../files/maglev_switch_breakout3_manual_1.00.pdf";
+a.download = "maglev_switch_breakout3_manual_1.00.pdf";
+a.style.display = 'none'
+a.click();
+
+a.addEventListener('click', function () {
+    window.close();
+});
