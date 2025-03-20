fec307566d3e6c8625bff2f553704dad6ec2a894
famichu <famichu@gmail.com>
2025-03-20 23:11:43 +0900

    fix links and update manuals

--- dl/scripts/man_refkb.js
+++ dl/scripts/man_refkb.js
@@ -1,6 +1,6 @@
 const a = document.createElement('a')
-a.href = "../files/maglev_switch_refkb_manual_1.01.pdf";
-a.download = "maglev_switch_refkb_manual_1.01.pdf";
+a.href = "../files/maglev_switch_refkb_manual_1.02.pdf";
+a.download = "maglev_switch_refkb_manual_1.02.pdf";
 a.style.display = 'none'
 a.click();
 
